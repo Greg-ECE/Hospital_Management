20360aedd40dd8df77fda3ed56c59e302f3879e3
Greg-ECE <gregoire.theard@edu.ece.fr>
2022-04-05 14:14:44 +0200

    Final Commit

--- Wpf_first_lab/First_app/Doctor_management.xaml.cs
+++ Wpf_first_lab/First_app/Doctor_management.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace First_app
 {
@@ -19,9 +21,42 @@ namespace First_app
     /// </summary>
     public partial class Doctor_management : Window
     {
+
+        DataClasses1DataContext db = new DataClasses1DataContext();
         public Doctor_management()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+         var data = (from x in db.Patient
+                    where x.PatientId == Convert.ToUInt32(code.Text)
+                    select x).FirstOrDefault();
+
+            name.Text = data.ToString();
+
+
+
+
+            //string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; Initial Catalog = Hospital; Integrated Security = True";
+            //SqlConnection connection = new SqlConnection(connectionString);
+            //string query = "INSERT INTO Doctors (DoctorId,DoctorName,DoctorTel,HiringDate,DoctorSpecialism) VALUES ('" + Int32.Parse(code.Text) + "','" + name.Text + "','" + Int32.Parse(tel.Text) + "','" + date.SelectedDate + "','" + speciality.Text +"') ";
+            //Console.WriteLine(query);
+            //SqlCommand command = new SqlCommand(query, connection);
+
+            //try
+            //{
+            //    connection.Open();
+            //    command.ExecuteNonQuery();
+            //    Console.WriteLine("Records Inserted Successfully");
+            //    connection.Close();
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Error : " + ex.Message);
+            //}
         }
     }
 }
